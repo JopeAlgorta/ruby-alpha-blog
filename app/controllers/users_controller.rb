28d945fda9418f0434efd5a3a0b9e952e5499841
Jose Algorta <jpalgort@gmail.com>
2022-01-27 15:46:36 -0300

    Add basic authentication functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}!"
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :articles
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
